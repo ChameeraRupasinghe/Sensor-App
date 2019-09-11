e867bc8ed8f9b147f9082f46dc9f54ddf4dfc7f3
ChameeraRupasinghe <chameera.15@cse.mrt.ac.lk>
2019-09-11 18:14:09 +0530

    Basic functionality implemented

--- app/src/main/java/com/example/sensorapp/MainActivity.java
+++ app/src/main/java/com/example/sensorapp/MainActivity.java
@@ -1,17 +1,66 @@
 package com.example.sensorapp;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private TextView textX, textY, textZ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager != null) {
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        }
+        textX = findViewById(R.id.textViewX);
+        textY = findViewById(R.id.textViewY);
+        textZ = findViewById(R.id.textViewZ);
     }
 
+    public void startReading(View view) {
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
+    public void stopReading(View view) {
+        sensorManager.unregisterListener(this);
+        textX.setText(R.string.text_x);
+        textY.setText(R.string.text_y);
+        textZ.setText(R.string.text_z);
+    }
 
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float x = sensorEvent.values[0];
+        float y = sensorEvent.values[1];
+        float z = sensorEvent.values[2];
+
+        textX.setText(String.valueOf(x));
+        textY.setText(String.valueOf(y));
+        textZ.setText(String.valueOf(z));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        System.out.println("Sensor accuracy changed to: " + i);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
 }
